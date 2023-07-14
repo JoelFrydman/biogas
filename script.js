537c002c2c8e453d31a03a6630d734249fd813ba
JoelFrydman <116125225+JoelFrydman@users.noreply.github.com>
2023-07-14 17:54:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-button").addEventListener("click", function() {
+    // Open a new email message in the user's email client.
+    window.open("mailto:info@biogaswebsite.com");
+  });
